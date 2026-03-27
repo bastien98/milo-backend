65234089c1d6e5534de663f095858d4935ed56ea
Pielie <gilles.1998.moenaert@gmail.com>
2026-03-27 08:42:36 +0100

    alembix fix

---
+++ migrations/versions/056_merge_heads.py
@@ -0,0 +1,26 @@
+"""merge_heads
+
+Revision ID: 056_merge_heads
+Revises: 055_add_charity_rejected_status, 054_merge_parallel_branches
+Create Date: 2026-03-27
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '056_merge_heads'
+down_revision: Union[str, None] = ('055_add_charity_rejected_status', '054_merge_parallel_branches')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
